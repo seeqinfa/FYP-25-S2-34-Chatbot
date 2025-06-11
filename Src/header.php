11b873b86aa0dad05af5dd88dda3ad5149d641ea
bloosaybleh <bladekoi99@gmail.com>
2025-06-12 00:55:30 +0800

    header created

---
+++ Src/header.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="logo">LuxFurniture</div>
+    <nav class="navigation">
+        <a href="#">Home</a>
+		<a href="#">About</a>
+		<a href="#">View Furniture</a>
+        <button class="btnLogin">Login</button>
+    </nav>
+</header>
